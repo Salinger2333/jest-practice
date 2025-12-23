f8345968f8563571bbe36e5369cefeb74edf7207
Salinger2333 <1565664814@qq.com>
2025-12-23 21:45:11 +0800

    wip: caesar cipher

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-function capitalize() {}
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-function reverseString() {}
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
 const calculator = {
-  add() {},
-  subtract() {},
-  divide() {},
-  multiply() {},
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
 };
 
-function caesarCipher(string, shift) {}
+function caesarCipher(string, shift) {
+  
+  string = string.split("");
+  return string.map
+}
 
 function analyzeArray() {}
 
--- index.test.js
+++ index.test.js
@@ -6,6 +6,35 @@ import {
   analyzeArray,
 } from "./index.js";
 
-test('check capitalize',() => {
-  
+test("check capitalize", () => {
+  expect(capitalize("qwerty")).toBe("Qwerty");
+  expect(capitalize("deep-seek")).toBe("Deep-seek");
+});
+test('check reverse',() => {
+  expect(reverseString("abcde")).toBe("edcba");
+  expect(reverseString('12345678')).toBe('87654321');
 })
+
+describe('check calculator', () => {
+  test('add', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+    expect(calculator.add(-1, 5)).toBe(4);
+    expect(calculator.add(0, 0)).toBe(0);
+  });
+
+  test('subtract', () => {
+    expect(calculator.subtract(5, 3)).toBe(2);
+  });
+
+  test('divide', () => {
+    expect(calculator.divide(10, 2)).toBe(5);
+  });
+
+  test('multiply', () => {
+    expect(calculator.multiply(2, 3)).toBe(6);
+  });
+});
+
+test('check caesar cypher', () => {
+  expect(caesarCipher('abcd',1)).toBe('bcde');
+});
