d26758d93d6c3984425e8e90c9b6445908fe4876
Salinger2333 <1565664814@qq.com>
2025-12-24 15:46:17 +0800

    Done

--- index.js
+++ index.js
@@ -22,11 +22,32 @@ const calculator = {
 };
 
 function caesarCipher(string, shift) {
-  
-  string = string.split("");
-  return string.map
+  shift = shift % 26;
+  let arrs = string.split("");
+  arrs = arrs.map((s) => {
+    let code = s.charCodeAt(0);
+    if (code >= 65 && code <= 90) {
+      return String.fromCharCode(((code - 65 + shift + 26) % 26) + 65);
+    }
+    if (code >= 97 && code <= 122) {
+      return String.fromCharCode(((code - 97 + shift + 26) % 26) + 97);
+    }
+    return s;
+  });
+  return arrs.join("");
 }
 
-function analyzeArray() {}
+function analyzeArray(array) {
+  const arr = [...array];
+  let sum = arr.reduce((prev, cur) => prev + cur, 0);
+  let min = arr.toSorted((a, b) => a - b)[0];
+  let max = arr.toSorted((a, b) => b - a)[0];
+  return {
+    average: sum / arr.length,
+    min,
+    max,
+    length: arr.length,
+  };
+}
 
 export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- index.test.js
+++ index.test.js
@@ -10,31 +10,45 @@ test("check capitalize", () => {
   expect(capitalize("qwerty")).toBe("Qwerty");
   expect(capitalize("deep-seek")).toBe("Deep-seek");
 });
-test('check reverse',() => {
+test("check reverse", () => {
   expect(reverseString("abcde")).toBe("edcba");
-  expect(reverseString('12345678')).toBe('87654321');
-})
+  expect(reverseString("12345678")).toBe("87654321");
+});
 
-describe('check calculator', () => {
-  test('add', () => {
+describe("check calculator", () => {
+  test("add", () => {
     expect(calculator.add(1, 2)).toBe(3);
     expect(calculator.add(-1, 5)).toBe(4);
     expect(calculator.add(0, 0)).toBe(0);
   });
 
-  test('subtract', () => {
+  test("subtract", () => {
     expect(calculator.subtract(5, 3)).toBe(2);
   });
 
-  test('divide', () => {
+  test("divide", () => {
     expect(calculator.divide(10, 2)).toBe(5);
   });
 
-  test('multiply', () => {
+  test("multiply", () => {
     expect(calculator.multiply(2, 3)).toBe(6);
   });
 });
 
-test('check caesar cypher', () => {
-  expect(caesarCipher('abcd',1)).toBe('bcde');
+test("check caesar cypher", () => {
+  expect(caesarCipher("abcd", 1)).toBe("bcde");
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+
+test("analyze array", () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+  console.log(object);
+  expect(object).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
